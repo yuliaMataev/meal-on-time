1a021b5bb12f178d00da8a3cc1adf761da9cece1
Yulia Mataev <yulia.mataev@gmail.com>
2022-11-14 21:07:59 +0200

    state,form

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,23 +6,40 @@ interface Props {
   category: string;
   rating: number;
 }
+
 function Card({ name, price, description, image, category, rating }: Props) {
-  /* const name = props.name; */
+  function handleClick(name: string) {
+    console.log(`Selected dish: ${name}`);
+  }
+
+  function handleCategoryClick(name: string) {
+    console.log(`Selected category: ${name}`);
+  }
+
   return (
-    <div className="card">
+    <div className="card m-4">
       <img src={image} className="card-img-top" alt={name} />
-      <div className="badge text-bg-info">{category}</div>
+      <div
+        onClick={() => handleCategoryClick(category)}
+        className="badge text-bg-info"
+      >
+        {category}
+      </div>
       <div className="card-body">
         <h5 className="card-title">
           <a href="#">{name}</a>
         </h5>
         <p className="card-text">{description}</p>
         <p className="card-text">{price}</p>
-        <p className="card-text"> Rating: {rating}</p>
-
-        <i className="bi-star "></i>
-        <br></br>
-        <a href="#" className="btn btn-primary">
+        <p className="card-text">
+          Rating: {rating}
+          <i className="bi-star"></i>
+        </p>
+        <a
+          onClick={(e) => handleClick(name)}
+          href="#"
+          className="btn btn-primary"
+        >
           Order Now
         </a>
       </div>
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import Card from "../../components/Card";
+import Title from "../../components/Title";
+import "./Home.css";
+
+interface CardType {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+  rating: number;
+}
+
+function Home() {
+  const data = [
+    {
+      id: 1,
+      name: "Greek Salad",
+      price: 30.5,
+      description: "Great salad...",
+      image:
+        "https://cdn.pixabay.com/photo/2016/08/09/10/30/,tomatoes-1580273__340.jpg",
+      category: "Vegeterian",
+      rating: 4,
+    },
+    {
+      id: 2,
+      name: "Pad Thai",
+      price: 42,
+      description: "Yumi yumi",
+      image:
+        "https://cdn.pixabay.com/photo/2017/02/25/15/23/pad-thai-2098017__340.jpg",
+      category: "Asian",
+      rating: 5,
+    },
+    {
+      id: 3,
+      name: "Fried Chicken",
+      price: 98.6,
+      description: "I like it",
+      image:
+        "https://cdn.pixabay.com/photo/2016/07/31/17/51/chicken-1559548__340.jpg",
+      category: "Chicken",
+      rating: 3,
+    },
+  ];
+
+  const catgories = ["All", "Vegeterian", "Asian", "Chicken"];
+
+  const [display, setDisplay] = useState("grid");
+  const [selectdCategory, setSelectdCategory] = useState("All");
+  const [filtered, setFiltered] = useState([...data]);
+
+  function filterByCategory(
+    category: string,
+    cards: Array<CardType>
+  ): Array<CardType> {
+    if (category === "All") {
+      return cards;
+    }
+
+    return cards.filter((card) => card.category === category);
+  }
+
+  function handleCategoryChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value;
+    const filteredData = filterByCategory(value, [...data]);
+
+    setSelectdCategory(value);
+    setFiltered(filteredData);
+  }
+
+  // function handleDisplayClick(displayType: string) {
+  //     setDisplay(displayType);
+  // }
+
+  return (
+    <>
+      <Title content="Order Delivery Or Takeaway" />
+
+      <div>
+        <button
+          onClick={() => setDisplay("grid")}
+          className="btn btn-light mx-1"
+        >
+          <i className="bi-grid-3x3-gap-fill"></i>
+        </button>
+
+        <button onClick={() => setDisplay("list")} className="btn btn-light">
+          <i className="bi-list-ul"></i>
+        </button>
+
+        <div>
+          <label>Category:</label>
+          <select
+            value={selectdCategory}
+            onChange={handleCategoryChange}
+            className="form-select"
+          >
+            {catgories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      <div className={`${display} p-5`}>
+        {filtered.map((card) => (
+          <Card key={card.id} {...card} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Home;
